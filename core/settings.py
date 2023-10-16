9d92043c13bd09b2bfead626e71365071c992114
Michel-Rooney <michelrooney16@gmail.com>
2023-10-16 19:14:15 -0300

    Flag

--- core/settings.py
+++ core/settings.py
@@ -161,7 +161,12 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.SessionAuthentication',
-    )
+    ),
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+        'rest_framework.renderers.AdminRenderer',
+    ]
 }
 
 # JWT - Json Web Token
